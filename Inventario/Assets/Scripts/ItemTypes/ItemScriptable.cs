99b54734788cbedaef4d4f26cc9330e835994475
Pedro Augusto <pbernardoguilherme@gmail.com>
2024-04-10 20:55:02 -0300

    ItemScriptable

---
+++ Inventario/Assets/Scripts/ItemTypes/ItemScriptable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScriptable : ScriptableObject
+{
+    public ItemType ItemType;
+    public string ItemName;
+    public string ItemDescription;
+    public string ItemLevel;
+}
