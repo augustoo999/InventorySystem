8bcce67446336b622c82c066353ab8dcfdbe3322
Pedro Augusto <pbernardoguilherme@gmail.com>
2024-04-11 02:33:34 -0300

    Update Feet.cs

--- Inventario/Assets/Scripts/ItemScripts/Feet.cs
+++ Inventario/Assets/Scripts/ItemScripts/Feet.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Feet : MonoBehaviour
+public class Feet : Armor
 {
-    // Start is called before the first frame update
-    void Start()
+    public int Hp;
+    public FeetDTO FeetDTO;
+
+    private void Start()
     {
-        
+        ManageDTO();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ManageDTO()
     {
-        
+        ItemName = FeetDTO.ItemName;
+        ItemDescription = FeetDTO.ItemDescription;
+        ItemLevel = FeetDTO.ItemLevel;
+        Hp = FeetDTO.Hp;
+        Mp = FeetDTO.Mp;
+        ItemType = FeetDTO.ItemType;
+    }
+
+    public override int GetDamage()
+    {
+        return 0;
+    }
+
+    public override int GetHp()
+    {
+        return Hp;
+    }
+
+    public override ItemDTO GetScriptable()
+    {
+        return FeetDTO;
     }
 }
