70ecddf9d597e9b68b535ef555991babe34e7572
Pedro Augusto <pbernardoguilherme@gmail.com>
2024-04-11 02:33:27 -0300

    Update HealthPotion.cs

--- Inventario/Assets/Scripts/ItemScripts/HealthPotion.cs
+++ Inventario/Assets/Scripts/ItemScripts/HealthPotion.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class HealthPotion : MonoBehaviour
+public class HealthPotion : Consumable, IPointerClickHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool _CanDoubleClick;
+    public int Hp;
+    public HealthPotionDTO HealthPotionDTO;
+
+
+    private void Start()
     {
-        
+        _CanDoubleClick = false;
+        ManageDTO();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ManageDTO()
     {
-        
+        ItemName = HealthPotionDTO.ItemName;
+        ItemDescription = HealthPotionDTO.ItemDescription;
+        ItemLevel = HealthPotionDTO.ItemLevel;
+        Hp = HealthPotionDTO.Hp;
+        ItemType = HealthPotionDTO.ItemType;
+    }
+
+    public override int GetDamage()
+    {
+        return 0;
+    }
+
+    public override int GetHp()
+    {
+        return Hp;
+    }
+
+    public override int GetMp()
+    {
+        return 0;
+    }
+
+    public override ItemDTO GetScriptable()
+    {
+        return HealthPotionDTO;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (_CanDoubleClick)
+        {
+            FindObjectOfType<EquipStats>().AddHpModifier(Hp);
+            CancelInvoke();
+            Destroy(gameObject);
+        }
+        else
+        {
+            _CanDoubleClick = true;
+            Invoke(nameof(ResetDoubleClick), 3f);
+        }
+    }
+
+    private void ResetDoubleClick()
+    {
+        _CanDoubleClick = false;
     }
 }
