5746023a443ea26fb40048b7570abed4d72e2353
Pedro Augusto <pbernardoguilherme@gmail.com>
2024-04-11 02:33:43 -0300

    Update Armor.cs

--- Inventario/Assets/Scripts/ItemScripts/Armor.cs
+++ Inventario/Assets/Scripts/ItemScripts/Armor.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Armor : MonoBehaviour
+public abstract class Armor : Item
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int Mp;
 
-    // Update is called once per frame
-    void Update()
+    public override int GetMp()
     {
-        
+        return Mp;
     }
 }
