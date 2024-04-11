69a7b77cec1e20d2d62f5a7a47d511b489deea64
Pedro Augusto <pbernardoguilherme@gmail.com>
2024-04-11 02:33:36 -0300

    Update DamagePotion.cs

--- Inventario/Assets/Scripts/ItemScripts/DamagePotion.cs
+++ Inventario/Assets/Scripts/ItemScripts/DamagePotion.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DamagePotion : MonoBehaviour
+public class DamagePotion : Consumable, IPointerClickHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool _CanDoubleClick;
+    public int Damage;
+    public DamagePotionDTO DamagePotionDTO;
+
+    private void Start()
     {
-        
+        _CanDoubleClick = false;
+        ManageDTO();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ManageDTO()
     {
-        
+        ItemName = DamagePotionDTO.ItemName;
+        ItemDescription = DamagePotionDTO.ItemDescription;
+        ItemLevel = DamagePotionDTO.ItemLevel;
+        Damage = DamagePotionDTO.Damage;
+        ItemType = DamagePotionDTO.ItemType;
     }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (_CanDoubleClick)
+        {
+            FindObjectOfType<EquipStats>().AddDamageModifier(Damage);
+            CancelInvoke();
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            _CanDoubleClick = true;
+            Invoke(nameof(ResetDoubleClick), 3f);
+        }
+    }
+
+    private void ResetDoubleClick()
+    {
+        _CanDoubleClick = false;
+    }
+
+    public override int GetDamage()
+    {
+        return Damage;
+    }
+
+    public override int GetHp()
+    {
+        return 0;
+    }
+
+    public override int GetMp()
+    {
+        return 0;
+    }
+
+    public override ItemDTO GetScriptable()
+    {
+        return DamagePotionDTO;
+    }
+
+    
 }
