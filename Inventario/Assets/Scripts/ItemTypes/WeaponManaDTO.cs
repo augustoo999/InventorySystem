66fbdfb77feddd1e94d75a1ba2d350011204ff9f
Pedro Augusto <pbernardoguilherme@gmail.com>
2024-04-11 02:31:47 -0300

    Script WeaponManaDTO updated

---
+++ Inventario/Assets/Scripts/ItemTypes/WeaponManaDTO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Weapon Mana", menuName = "Item/Weapon/Weapon Mana")]
+public class WeaponManaDTO : WeaponDTO
+{
+    public int Mp;
+}
