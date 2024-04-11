280349a32840212fb4d8211f93bb62f3db73032a
Pedro Augusto <pbernardoguilherme@gmail.com>
2024-04-11 02:33:04 -0300

    Script Weapon Updated

--- Inventario/Assets/Scripts/ItemScripts/Weapon.cs
+++ Inventario/Assets/Scripts/ItemScripts/Weapon.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public abstract class Weapon : Item 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int Damage;
 
-    // Update is called once per frame
-    void Update()
+    public override int GetDamage()
     {
-        
+        return Damage;
     }
 }
