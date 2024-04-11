c5534bcff9e31ad7f533c6f3389f15cf39d7d81a
Pedro Augusto <pbernardoguilherme@gmail.com>
2024-04-11 02:33:24 -0300

    Script ManaPotion Updated

--- Inventario/Assets/Scripts/ItemScripts/ManaPotion.cs
+++ Inventario/Assets/Scripts/ItemScripts/ManaPotion.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ManaPotion : MonoBehaviour
+public class ManaPotion : Consumable, IPointerClickHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool _CanDoubleClick;
+    public int Mp;
+    public ManaPotionDTO ManaPotionDTO;
+
+    private void Start()
     {
-        
+        _CanDoubleClick = false;
+        ManageDTO();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerClick(PointerEventData eventData)
     {
-        
+       if (_CanDoubleClick)
+        {
+            FindObjectOfType<EquipStats>().AddMpModifier(Mp);
+            CancelInvoke();
+            Destroy(gameObject);
+        }
+        else
+        {
+            _CanDoubleClick = true;
+            Invoke(nameof(ResetDoubleClick), 3f);
+        }
+    }
+
+    public void ResetDoubleClick()
+    {
+        _CanDoubleClick = false;
+    }
+
+    private void ManageDTO()
+    {
+        ItemName = ManaPotionDTO.ItemName;
+        ItemDescription = ManaPotionDTO.ItemDescription;
+        ItemLevel = ManaPotionDTO.ItemLevel;
+        Mp = ManaPotionDTO.Mp;
+        ItemType = ManaPotionDTO.ItemType;
+    }
+
+    public override int GetDamage()
+    {
+        return 0;
+    }
+
+    public override int GetHp()
+    {
+        return 0;
+    }
+
+    public override int GetMp()
+    {
+        return Mp;
+    }
+
+    public override ItemDTO GetScriptable()
+    {
+        return ManaPotionDTO;
     }
 }
